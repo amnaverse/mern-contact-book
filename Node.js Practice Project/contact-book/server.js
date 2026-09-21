fc6f568e30733497876687e8c8a68c81cbf97eb1
amnaverse <amna16528@gmail.com>
2026-09-21 11:09:06 -0700

    Prepare backend for deployment

--- Node.js Practice Project/contact-book/server.js
+++ Node.js Practice Project/contact-book/server.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(cors());
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/contactBookDB')
+mongoose.connect(process.env.MONGO_URI || 'mongodb://amna16528_db_user:BLDvdE1qRnExSDE1@ac-wwyvqqs-shard-00-00.whyr94x.mongodb.net:27017,ac-wwyvqqs-shard-00-01.whyr94x.mongodb.net:27017,ac-wwyvqqs-shard-00-02.whyr94x.mongodb.net:27017/contactBookDB?ssl=true&replicaSet=atlas-lqf3wd-shard-0&authSource=admin&appName=Cluster0')
   .then(() => console.log('Connected to MongoDB!'))
   .catch((err) => console.log('Connection error:', err));
 
@@ -56,6 +56,7 @@ app.delete('/contacts/:name', async (req, res) => {
   res.json({ message: 'Contact deleted!' });
 });
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
   console.log('Server running on http://localhost:3000');
 });
