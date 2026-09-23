29bd97e2885c85f5a30f54f86ab15ca73983aac3
amnaverse <amna16528@gmail.com>
2026-09-23 16:56:34 -0700

    Remove hardcoded MongoDB credentials

--- Node.js Practice Project/contact-book/server.js
+++ Node.js Practice Project/contact-book/server.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(cors());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI || 'mongodb://amna16528_db_user:BLDvdE1qRnExSDE1@ac-wwyvqqs-shard-00-00.whyr94x.mongodb.net:27017,ac-wwyvqqs-shard-00-01.whyr94x.mongodb.net:27017,ac-wwyvqqs-shard-00-02.whyr94x.mongodb.net:27017/contactBookDB?ssl=true&replicaSet=atlas-lqf3wd-shard-0&authSource=admin&appName=Cluster0')
+mongoose.connect(mongoose.connect(process.env.MONGO_URI))
   .then(() => console.log('Connected to MongoDB!'))
   .catch((err) => console.log('Connection error:', err));
 
